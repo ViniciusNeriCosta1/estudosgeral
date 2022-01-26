8752b968b24669dedd941b5763a617fad31938cc
Vinicius Neri <vinicius.neriii@gmail.com>
2022-01-26 12:00:00 -0300

    começando classe

---
+++ web_mdrn/funcao/cidadaoPrimeiraClasse.js
@@ -0,0 +1,8 @@
+// criar uma funcao de forma literal
+function fun1(){ }
+
+// Armazenar uma funcao numa variavel
+const fun2 = function() { }
+
+// Armazenar uma funcao num array
+const array = [function(a,b) {return a + b }, fun1, fun2]
