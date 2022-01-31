7e29ace6f3ce17a5dabf058f825773b354f16c16
Vinicius Neri <vinicius.neriii@gmail.com>
2022-01-31 14:04:05 -0300

    exemplo classe

--- web_mdrn/funcao/cidadaoPrimeiraClasse.js
+++ web_mdrn/funcao/cidadaoPrimeiraClasse.js
@@ -6,3 +6,24 @@ const fun2 = function() { }
 
 // Armazenar uma funcao num array
 const array = [function(a,b) {return a + b }, fun1, fun2]
+
+// Armazenar em um atributo de objeto
+const obj = {}
+obj.falar = function() {return 'Opa'}
+
+// Armazenar função como param
+function run(fun){fun()}
+
+run(function(){console.log('executando...')})
+
+// Uma função pode retornar/conter uma funcao
+function soma(a,b){
+    return function(c){
+        console.log(a+b+c)
+    }
+}
+
+soma(2, 3)(4)
+
+const cincoMais = soma(2,3)
+cincoMais(4)
