4da4e1f7e837f1bc32f1fb4fc9eb379dd5310b73
Vinicius Neri <vinicius.neriii@gmail.com>
2022-03-14 22:16:55 -0300

    aula 1 alura

---
+++ alura/aula1/calcMed.js
@@ -0,0 +1,8 @@
+function conversao(){
+    const dolar = window.document.getElementById('dolar')
+    const dolarNumber = Number(dolar.value) * 5.51
+    
+    let resultado = window.document.getElementById('resultado')
+    
+    resultado.innerHTML = parseFloat((dolarNumber).toFixed(2))
+  }
